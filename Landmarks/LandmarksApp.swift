50829696e03bb51d913fb70a024a915d18f586cc
Jeremy Kim <jedo@alumni.unc.edu>
2025-04-12 23:52:20 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jeremy Kim on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,16 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by Jeremy Kim on 4/12/25.
+//
+
+import Testing
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
